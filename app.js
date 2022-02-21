495a1eacacad97a97621f405ca55ccee7e9c6fbb
Kacper <kapmid123@gmail.com>
2022-02-21 22:30:03 +0100

    css and js changes

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const expenseInput = document.querySelector("#expense-input");
 const expenseBtn = document.querySelector("#expense-btn");
 const expense = document.querySelector("#expenses");
 const balance = document.querySelector("#balance");
+const expensesTitle = document.querySelector("#expenses-title");
+const expenseTitleInput = document.querySelector("#expense-title-input");
+const expensesValue = document.querySelector("#expenses-value");
+const deleteIcon = document.querySelector("#delete-icon")
 let budgetValue = 0;
 let result = 0;
 let expenseSum = 0;
@@ -27,12 +31,39 @@ function AddExpenses(){
     console.log(expenseSum);
     expense.innerHTML = "$ " + expenseSum;
     Balance();
+    BalanceColor();
+    createExpenditure();
 }
 
 function Balance(){
     
     result = result - expenseInput.value; 
     console.log(result);
-    balance.innerHTML = "$ " + result;
+    balance.innerHTML = "$ " + result.toFixed(2);
 }
 
+function BalanceColor(){
+    if(result > 0){
+        balance.style.color ="#42FF00";
+    }
+    else if ( result === 0 ){
+        balance.style.color ="#FFFFFF";
+    }
+    else{
+        balance.style.color ="#FF0000";
+    }
+}
+
+function createExpenditure(){
+
+
+    const valueElement = document.createElement("div");
+    valueElement.innerHTML = 
+    `<p>${expenseTitleInput.value}</p>`+
+    `<p>${expenseInput.value}</p>`+
+    `<section><img src="icon/pen-to-square-solid.png" alt=""> <img src="icon/trash-solid.png" alt=""></section>`;
+    expensesValue.appendChild(valueElement);
+    
+
+
+}
