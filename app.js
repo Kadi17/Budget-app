607e1d822670eab16826d6129a304c98d5bd9dcf
Kadi17 <kapmid123@gmail.com>
2022-02-22 09:52:43 +0100

    fix: nan value, formatting

--- app.js
+++ app.js
@@ -5,65 +5,65 @@ const expenseInput = document.querySelector("#expense-input");
 const expenseBtn = document.querySelector("#expense-btn");
 const expense = document.querySelector("#expenses");
 const balance = document.querySelector("#balance");
-const expensesTitle = document.querySelector("#expenses-title");
 const expenseTitleInput = document.querySelector("#expense-title-input");
 const expensesValue = document.querySelector("#expenses-value");
-const deleteIcon = document.querySelector("#delete-icon")
+let deleteBtns;
 let budgetValue = 0;
 let result = 0;
 let expenseSum = 0;
 
+budgetBtn.addEventListener("click", AddBudget);
+expenseBtn.addEventListener("click", AddExpenses);
 
-budgetBtn.addEventListener('click', AddBudget);
-expenseBtn.addEventListener('click', AddExpenses);
+function AddBudget() {
+  budgetValue = parseInt(budgetInput.value);
 
-function AddBudget()
-{
-    budgetValue = parseInt(budgetInput.value);
-    budget.innerHTML ="$ " + budgetValue;
-    budgetInput.value = "";
-    result = budgetValue;
-    
+  if (!budgetValue) return;
+
+  budget.innerHTML = "$ " + budgetValue;
+  budgetInput.value = "";
+  result = budgetValue;
 }
 
-function AddExpenses(){
-    expenseSum += parseInt(expenseInput.value);
-    console.log(expenseSum);
-    expense.innerHTML = "$ " + expenseSum;
-    Balance();
-    BalanceColor();
-    createExpenditure();
+function AddExpenses() {
+  expenseSum += parseInt(expenseInput.value);
+  console.log(expenseSum);
+  expense.innerHTML = "$ " + expenseSum;
+  Balance();
+  BalanceColor();
+  createExpenditure();
+  deleteBtns = document.querySelectorAll(".delete-btn");
+  for (const btn of deleteBtns) {
+    btn.addEventListener("click", deleteExpense);
+  }
 }
 
-function Balance(){
-    
-    result = result - expenseInput.value; 
-    console.log(result);
-    balance.innerHTML = "$ " + result.toFixed(2);
+function Balance() {
+  result = result - expenseInput.value;
+  console.log(result);
+  balance.innerHTML = "$ " + result.toFixed(2);
 }
 
-function BalanceColor(){
-    if(result > 0){
-        balance.style.color ="#42FF00";
-    }
-    else if ( result === 0 ){
-        balance.style.color ="#FFFFFF";
-    }
-    else{
-        balance.style.color ="#FF0000";
-    }
+function BalanceColor() {
+  if (result > 0) {
+    balance.style.color = "#42FF00";
+  } else if (result === 0) {
+    balance.style.color = "#FFFFFF";
+  } else {
+    balance.style.color = "#FF0000";
+  }
 }
 
-function createExpenditure(){
+function createExpenditure() {
+  const valueElement = document.createElement("tr");
+  valueElement.innerHTML =
+    `<td>${expenseTitleInput.value}</td>` +
+    `<td>${expenseInput.value}</td>` +
+    `<td><img src="style/icon/pen-to-square-solid.png" alt="">  <img class="delete-btn" src="style/icon/trash-solid.png" alt=""></td>`;
+  expensesValue.appendChild(valueElement);
+}
 
-
-    const valueElement = document.createElement("div");
-    valueElement.innerHTML = 
-    `<p>${expenseTitleInput.value}</p>`+
-    `<p>${expenseInput.value}</p>`+
-    `<section><img src="style/icon/pen-to-square-solid.png" alt=""> <img src="style/icon/trash-solid.png" alt=""></section>`;
-    expensesValue.appendChild(valueElement);
-    
-
-
+function deleteExpense() {
+  this.parentElement.parentElement.remove();
+  console.log();
 }
